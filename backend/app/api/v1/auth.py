1203a25a720ed4b2bfba4fd92aa5dabcf29a9af0
hector <h.rosales01@ufromail.cl>
2026-01-30 18:10:32 -0300

    refactor: endpoint register now use directly user_service.create

--- backend/app/api/v1/auth.py
+++ backend/app/api/v1/auth.py
@@ -1,15 +1,16 @@
 from fastapi import APIRouter, Depends
 from fastapi.security import OAuth2PasswordRequestForm
+from app.services.user_service import user_service
 from app.services.auth_service import auth_service
 from app.models.token import Token
-from app.models.user import UserRegister
+from app.models.user import UserRegister, UserResponse
 from app.db.database import GetSession
 
 router = APIRouter()
 
-@router.post("/register", response_model=dict)
+@router.post("/register", response_model=UserResponse)
 async def register(user_in: UserRegister, db: GetSession):
-    return await auth_service.register(db, user_in)
+    return await user_service.create(db, user_in)
 
 
 @router.post("/login", response_model=Token)
